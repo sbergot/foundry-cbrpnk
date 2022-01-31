c1e5170c4f7d8216b679405f7f6c73fe46589fc5
Simon Bergot <sbergot@talentsoft.com>
2022-01-31 14:29:34 +0100

    wip action roll

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -1,11 +1,20 @@
-import {onManageActiveEffect, prepareActiveEffectCategories} from "../helpers/effects.mjs";
+import {
+  onManageActiveEffect,
+  prepareActiveEffectCategories,
+} from "../helpers/effects.mjs";
+
+const RollStatus = {
+  failure: "failure",
+  partial: "partial",
+  success: "success",
+  critical: "critical"
+}
 
 /**
  * Extend the basic ActorSheet with some very simple modifications
  * @extends {ActorSheet}
  */
 export class CbrpnkActorSheet extends ActorSheet {
-
   /** @override */
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
@@ -13,7 +22,13 @@ export class CbrpnkActorSheet extends ActorSheet {
       template: "systems/cbrpnk/templates/actor/actor-sheet.html",
       width: 600,
       height: 600,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "features" }]
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "features",
+        },
+      ],
     });
   }
 
@@ -40,13 +55,13 @@ export class CbrpnkActorSheet extends ActorSheet {
     context.flags = actorData.flags;
 
     // Prepare character data and items.
-    if (actorData.type == 'character') {
+    if (actorData.type == "character") {
       this._prepareItems(context);
       this._prepareCharacterData(context);
     }
 
     // Prepare NPC data and items.
-    if (actorData.type == 'npc') {
+    if (actorData.type == "npc") {
       this._prepareItems(context);
     }
 
@@ -94,22 +109,22 @@ export class CbrpnkActorSheet extends ActorSheet {
       6: [],
       7: [],
       8: [],
-      9: []
+      9: [],
     };
 
     // Iterate through items, allocating to containers
     for (let i of context.items) {
       i.img = i.img || DEFAULT_TOKEN;
       // Append to gear.
-      if (i.type === 'item') {
+      if (i.type === "item") {
         gear.push(i);
       }
       // Append to features.
-      else if (i.type === 'feature') {
+      else if (i.type === "feature") {
         features.push(i);
       }
       // Append to spells.
-      else if (i.type === 'spell') {
+      else if (i.type === "spell") {
         if (i.data.spellLevel != undefined) {
           spells[i.data.spellLevel].push(i);
         }
@@ -129,7 +144,7 @@ export class CbrpnkActorSheet extends ActorSheet {
     super.activateListeners(html);
 
     // Render the item sheet for viewing/editing prior to the editable check.
-    html.find('.item-edit').click(ev => {
+    html.find(".item-edit").click((ev) => {
       const li = $(ev.currentTarget).parents(".item");
       const item = this.actor.items.get(li.data("itemId"));
       item.sheet.render(true);
@@ -140,10 +155,10 @@ export class CbrpnkActorSheet extends ActorSheet {
     if (!this.isEditable) return;
 
     // Add Inventory Item
-    html.find('.item-create').click(this._onItemCreate.bind(this));
+    html.find(".item-create").click(this._onItemCreate.bind(this));
 
     // Delete Inventory Item
-    html.find('.item-delete').click(ev => {
+    html.find(".item-delete").click((ev) => {
       const li = $(ev.currentTarget).parents(".item");
       const item = this.actor.items.get(li.data("itemId"));
       item.delete();
@@ -151,15 +166,17 @@ export class CbrpnkActorSheet extends ActorSheet {
     });
 
     // Active Effect management
-    html.find(".effect-control").click(ev => onManageActiveEffect(ev, this.actor));
+    html
+      .find(".effect-control")
+      .click((ev) => onManageActiveEffect(ev, this.actor));
 
     // Rollable abilities.
-    html.find('.rollable').click(this._onRoll.bind(this));
+    html.find(".rollable").click(this._onRoll.bind(this));
 
     // Drag events for macros.
     if (this.actor.isOwner) {
-      let handler = ev => this._onDragStart(ev);
-      html.find('li.item').each((i, li) => {
+      let handler = (ev) => this._onDragStart(ev);
+      html.find("li.item").each((i, li) => {
         if (li.classList.contains("inventory-header")) return;
         li.setAttribute("draggable", true);
         li.addEventListener("dragstart", handler, false);
@@ -185,13 +202,13 @@ export class CbrpnkActorSheet extends ActorSheet {
     const itemData = {
       name: name,
       type: type,
-      data: data
+      data: data,
     };
     // Remove the type from the dataset since it's in the itemData.type prop.
     delete itemData.data["type"];
 
     // Finally, create the item!
-    return await Item.create(itemData, {parent: this.actor});
+    return await Item.create(itemData, { parent: this.actor });
   }
 
   /**
@@ -199,31 +216,91 @@ export class CbrpnkActorSheet extends ActorSheet {
    * @param {Event} event   The originating click event
    * @private
    */
-  _onRoll(event) {
+  async _onRoll(event) {
     event.preventDefault();
     const element = event.currentTarget;
     const dataset = element.dataset;
 
     // Handle item rolls.
     if (dataset.rollType) {
-      if (dataset.rollType == 'item') {
-        const itemId = element.closest('.item').dataset.itemId;
+      if (dataset.rollType == "item") {
+        const itemId = element.closest(".item").dataset.itemId;
         const item = this.actor.items.get(itemId);
         if (item) return item.roll();
+      }
+      if (dataset.rollType == "action") {
+        let label = dataset.label ? dataset.label : "";
+        const value = this.evaluateFormula(dataset.rollValue);
+        let formula = "";
+        const zeroSkill = value <= 0;
+        if (zeroSkill) {
+          formula = "2d6";
+        } else {
+          formula = `${value}d6`;
+        }
+        const roll = new Roll(formula);
+        await roll.evaluate({async:true});
+        this.showActionRoll(roll, zeroSkill);
+        // roll.toMessage({
+        //   speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+        //   flavor: label,
+        //   rollMode: game.settings.get("core", "rollMode"),
+        // });
+        return roll;
       }
     }
 
     // Handle rolls that supply the formula directly.
     if (dataset.roll) {
-      let label = dataset.label ? `[ability] ${dataset.label}` : '';
+      let label = dataset.label ? dataset.label : "";
       let roll = new Roll(dataset.roll, this.actor.getRollData());
       roll.toMessage({
         speaker: ChatMessage.getSpeaker({ actor: this.actor }),
         flavor: label,
-        rollMode: game.settings.get('core', 'rollMode'),
+        rollMode: game.settings.get("core", "rollMode"),
       });
       return roll;
     }
   }
 
+  evaluateFormula(formula) {
+    return parseInt(Roll.replaceFormulaData(formula, this.actor.getRollData()));
+  }
+
+  async showActionRoll(rolls, zeroSkill) {
+    // we are always rolling Nd6 so we have exactly 1 DiceTerm
+    const results = rolls.dice[0].results.map(r => r.result);
+    let main_result = null;
+    let second_result = null;
+    if (zeroSkill) {
+      results.sort((a, b) => a - b); // ascending sort
+      main_result = results[0];
+      second_result = 0;
+    } else {
+      results.sort((a, b) => b - a); // descending sort
+      main_result = results[0];
+      second_result = results[1] ?? 0;
+    }
+
+    let roll_status = '';
+    if (main_result <= 3) {
+      roll_status = RollStatus.failure;
+    } else if (main_result <= 5) {
+      roll_status = RollStatus.partial;
+    } else if (main_result == 6) {
+      roll_status = second_result == 6
+        ? RollStatus.critical
+        : RollStatus.success;
+    }
+
+    const html = await renderTemplate("systems/cbrpnk/templates/chat/action-roll.html", { roll_status, rolls });
+    let messageData = {
+      speaker: ChatMessage.getSpeaker(),
+      content: html,
+      type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+      roll: rolls
+    }
+  
+    CONFIG.ChatMessage.documentClass.create(messageData, {})
+  }
 }
